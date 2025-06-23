5a50e582a1a804e03fef7a6dd7372639866f9e12
rey <reyarby.castro@gmail.com>
2025-06-23 18:32:38 +0800

    fixed error: greeting and name in chatbot_data

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from ChatBot import MeowBot
 import sys
 
@@ -12,19 +11,4 @@ def main():
     chatbot.chat()
 
 if __name__ == "__main__":
-=======
-from chatbot import MeowBot
-import sys
-
-def main():
-    if hasattr(sys.stdout, "reconfigure"):
-        sys.stdout.reconfigure(encoding='utf-8')
-        
-    print("🚀 Starting MeowBot...")
-    
-    chatbot = MeowBot()
-    chatbot.chat()
-
-if __name__ == "__main__":
->>>>>>> 1dd03d383ad3a53fd9f8dbdbb61a042fced9b622
     main()
